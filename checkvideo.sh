3b017976c5e928a589dd9212a0404087d5b67476
modeverv <modeverv@gmail.com>
2015-11-25 01:44:51 +0900

    add checkvideo.sh

---
+++ checkvideo.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo $PATH
+PATH=$PATH:/home/seijiro/.nvm/v0.8.4/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+echo `date`
+echo $PATH
+cd /home/seijiro/crawler
+# source /home/seijiro/.rvm/environments/ruby-2.0.0-p0@rails
+PID=`ps x | grep -v grep | grep "crawler.rb" | awk '{ print $1 }'`
+if [ x"$PID" != x"" ]; then
+    kill -9 $PID
+fi
+bundle exec ruby -W0 checkvideo.rb
