bf617833dd4a97fee981f91910dc1f93bcbe2590
modeverv <modeverv@gmail.com>
2015-11-26 22:22:00 +0900

    little fix at shell script

--- checkvideo.sh
+++ checkvideo.sh
@@ -5,7 +5,7 @@ echo `date`
 echo $PATH
 cd /home/seijiro/crawler
 # source /home/seijiro/.rvm/environments/ruby-2.0.0-p0@rails
-PID=`ps x | grep -v grep | grep "crawler.rb" | awk '{ print $1 }'`
+PID=`ps x | grep -v grep | grep "checkvideo.rb" | awk '{ print $1 }'`
 if [ x"$PID" != x"" ]; then
     kill -9 $PID
 fi
